7422d0a4ed8fea63b085da874789c396db55e906
saforer <saforer@gmail.com>
2018-07-06 17:56:19 -0400

    Prototype Game made
    Used imported model and setup to have physics using the playerscript script. We got ourselves a start enough for tomr to use I believe.

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+    Rigidbody2D rb;
+    public float thrustSpeed = .05f;
+    public float rotSpeed = 1f;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Thrust(1);
+        } else if (Input.GetKey(KeyCode.S)) {
+            Thrust(-1);
+        }
+
+
+		if (Input.GetKey(KeyCode.A))
+        {
+            Rotate(1);
+        } else if (Input.GetKey(KeyCode.D))
+        {
+            Rotate(-1);
+        }
+	}
+
+    void Thrust(int direction)
+    {
+        Vector3 vel = rb.velocity;
+        vel += gameObject.transform.up * thrustSpeed * direction;
+        rb.velocity = vel;
+    }
+
+    void Rotate(int direction)
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * direction * rotSpeed);
+    }
+}
