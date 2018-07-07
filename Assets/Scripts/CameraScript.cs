b71fa9abcce4e5cfea1ae17e5fe9ce5faec54aa2
TomRintjema <tomrintjema@hotmail.com>
2018-07-07 14:43:59 -0400

    Tweaks
    I tried to make the camera follow the movement of the ship but stopped because I don't think it's good anyway.
    Also changed values on stuff etc.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
     GameObject playerShip;
+    //private Rigidbody2D playerRididbody2D;
 
 	// Use this for initialization
 	void Start () {
         playerShip = GameObject.FindWithTag("Player");
+        //playerRididbody2D = playerShip.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        gameObject.transform.position = new Vector3 (playerShip.transform.position.x, playerShip.transform.position.y, -10);
-	}
+
+        gameObject.transform.position = new Vector3(playerShip.transform.position.x, playerShip.transform.position.y, -10);
+        //gameObject.transform.position = new Vector3(playerShip.transform.position.x + playerRididbody2D.velocity.x, playerShip.transform.position.y + playerRididbody2D.velocity.y, -10);
+    }
 }
