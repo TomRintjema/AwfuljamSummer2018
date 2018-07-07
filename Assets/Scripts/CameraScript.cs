0b012f51197c8fad5301f3a63c2c9dfee19677bf
saforer <saforer@gmail.com>
2018-07-07 10:14:03 -0400

    Camera Attach to Player
    Make sure ship is tagged "Player" and the camera will follow it closely

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+    GameObject playerShip;
+
+
+	// Use this for initialization
+	void Start () {
+        playerShip = GameObject.FindWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = new Vector3 (playerShip.transform.position.x, playerShip.transform.position.y, -10);
+	}
+}
