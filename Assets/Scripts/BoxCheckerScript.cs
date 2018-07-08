bd02339295277abc99f180da86ec53878ca03b07
saforer <saforer@gmail.com>
2018-07-08 12:01:09 -0400

    Two changes
    Stuff breaks if it hits too hard
    Boxes are detected when stopped in a zone

---
+++ Assets/Scripts/BoxCheckerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxCheckerScript : MonoBehaviour {
+    public float threshhold;
+    void OnTriggerStay2D (Collider2D other)
+    {
+        if (other.gameObject.tag == "Box") {
+            if (other.gameObject.GetComponent<ContainerScript>().locked == false)
+            {
+
+                if (!(other.gameObject.GetComponent<Rigidbody2D>().velocity.y > threshhold) &&
+                    !(other.gameObject.GetComponent<Rigidbody2D>().velocity.y < -threshhold))
+                {
+                    Debug.Log("BOX IS STOPPED AND IN THE HOLE VELOCITY IS " + other.gameObject.GetComponent<Rigidbody2D>().velocity.y);
+                    other.gameObject.GetComponent<ContainerScript>().locked = true;
+                }
+            }
+        }
+    }
+}
