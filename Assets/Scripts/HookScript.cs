c8b6322456996b93b3671b2d90fc111dc50673a8
saforer <saforer@gmail.com>
2018-07-08 16:17:22 -0400

    Beta of hook in
    Still needs a lot of work, retraction extension appearing and disappearing BUT IT'S SOMETHIGN TO PLAY WITH

---
+++ Assets/Scripts/HookScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookScript : MonoBehaviour {
+    GameObject ship;
+    public bool hooking = true;
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hooking)
+        {
+            if (collision.gameObject.tag == "Box")
+            {
+                GameObject box = collision.gameObject;
+                box.transform.parent = gameObject.transform;
+                gameObject.GetComponent<DistanceJoint2D>().connectedBody = box.GetComponent<Rigidbody2D>();
+                gameObject.GetComponent<DistanceJoint2D>().enabled = true;
+            }
+        }
+    }
+}
