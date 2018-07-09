338b1494201ab272f2448a9dfd229510249b88b8
TomRintjema <tomrintjema@hotmail.com>
2018-07-08 20:20:16 -0400

    AutoGround
    Made the ground auto fill under the tiles when using the auto ground prefabs.
    Changed the graphics on the ship's chain. The scale of the game objects was 0.1 or 0.01 so when I put it back to 1 things stopped working. I fixed what I could but it still needs work. Sorry Forer.

---
+++ Assets/Scripts/GroundStretch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundStretch : MonoBehaviour {
+    public float tileHeight = 2f;
+
+    // Use this for initialization
+    void Start() {
+        Vector3 newScale = new Vector3(1, transform.position.y * 0.5f, 1);
+        transform.localScale = newScale;
+        Vector3 newPosition = new Vector3(transform.position.x, (transform.position.y * 0.5f) - tileHeight, transform.position.z);
+        transform.position = newPosition;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
