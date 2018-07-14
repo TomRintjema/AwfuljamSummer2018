ae02f233890cee47aa07f2e0612be470b673edb6
TomRintjema <tomrintjema@hotmail.com>
2018-07-14 18:33:37 -0400

    Game can restart now

---
+++ Assets/TeamNameAssets/Scripts/RestartGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour {
+
+    private void Awake()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void ReloadMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
