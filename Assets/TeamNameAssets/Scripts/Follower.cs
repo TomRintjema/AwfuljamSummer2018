e5f25eb4f55048d6a40a941b9306f2478e91d03f
TomRintjema <tomrintjema@hotmail.com>
2018-07-14 21:35:48 -0400

    Ground follows camera now

---
+++ Assets/TeamNameAssets/Scripts/Follower.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour {
+    public Transform target;
+    public bool x;
+    public bool y;
+    public bool z;
+    private Vector3 targetPos;
+
+	// Use this for initialization
+	void Start () {
+        targetPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (x)
+        {
+            targetPos.x = target.position.x;
+        }
+        if(y)
+        {
+            targetPos.y = target.position.y;
+        }
+        if(z)
+        {
+            targetPos.z = target.position.z;
+        }
+        transform.position = targetPos;
+    }
+}
