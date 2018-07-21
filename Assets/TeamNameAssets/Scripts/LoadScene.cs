a07fe56557df126d7a7fb7c0b840c272098809e5
TomRintjema <tomrintjema@hotmail.com>
2018-07-21 10:46:42 -0400

    Music doesn't restart when you restart game

---
+++ Assets/TeamNameAssets/Scripts/LoadScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
